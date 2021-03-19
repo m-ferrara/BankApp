bde085e4ee825b354a7b381b53fbcd4ae43ae0ec
Matt Ferrara <mattf@magenic.com>
2021-03-19 02:08:40 -0500

    Add Employee Skill controllers, services, entities, repositories, configure swagger and actuators

--- src/main/java/com/groupthree/bankapp/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.groupthree.bankapp.service;
-
-import com.groupthree.bankapp.entity.User;
-
-public interface UserService {
-    void deleteById(long id);
-
-    User save(User user);
-}
